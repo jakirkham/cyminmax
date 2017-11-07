d8e7b48881f96b08f1d503d7124b331b6891d855
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-11-07 12:36:07 -0500

    Add a NumPy build extension command class
    Ensures that we can still add NumPy's headers to the extensions we plan
    to build even though NumPy has not been installed via setup requirements
    on the first pass of `setup.py`. Does this by overriding the `build_ext`
    command class to add in the NumPy headers as part of option
    finalization. Should make it easier for anyone trying to install this
    package without NumPy already installed.

--- setup.py
+++ setup.py
@@ -7,11 +7,21 @@ from glob import glob
 
 import setuptools
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as BuildExtCommand
 from setuptools.command.test import test as TestCommand
 
 from distutils.sysconfig import get_config_var, get_python_inc
 
 import versioneer
+
+
+class NumPyBuildExt(BuildExtCommand):
+    def finalize_options(self):
+        BuildExtCommand.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 
 class PyTest(TestCommand):
@@ -48,6 +58,7 @@ test_requirements = [
 
 cmdclasses = dict()
 cmdclasses.update(versioneer.get_cmdclass())
+cmdclasses["build_ext"] = NumPyBuildExt
 cmdclasses["test"] = PyTest
 
 
