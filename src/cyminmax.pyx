86aa78199b8d95b64fa12c53e85456b54a5e25cb
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-11-15 14:45:23 -0500

    Store the `arr`'s size
    As we need `arr`'s size for the later call to `cminmax`, go ahead and
    store the result the first time it is requested to verify the array is
    non-empty. This should remove the cost from these later size checks.

--- src/cyminmax.pyx
+++ src/cyminmax.pyx
@@ -25,7 +25,9 @@ def minmax(a):
 
     arr = numpy.asanyarray(a)
 
-    if not numpy.PyArray_SIZE(arr):
+    cdef size_t arr_size = numpy.PyArray_SIZE(arr)
+
+    if not arr_size:
         raise ValueError(
             "zero-size array to reduction operation minmax which has no "
             "identity"
@@ -41,55 +43,55 @@ def minmax(a):
     cdef numpy.NPY_TYPES arr_dtype_num = arr.dtype.num
     if arr_dtype_num == numpy.NPY_BOOL:
         cyminmax.cminmax[numpy.npy_bool](
-            <numpy.npy_bool*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_bool*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_UBYTE:
         cyminmax.cminmax[numpy.npy_ubyte](
-            <numpy.npy_ubyte*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_ubyte*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_USHORT:
         cyminmax.cminmax[numpy.npy_ushort](
-            <numpy.npy_ushort*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_ushort*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_UINT:
         cyminmax.cminmax[numpy.npy_uint](
-            <numpy.npy_uint*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_uint*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_ULONG:
         cyminmax.cminmax[numpy.npy_ulong](
-            <numpy.npy_ulong*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_ulong*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_ULONGLONG:
         cyminmax.cminmax[numpy.npy_ulonglong](
-            <numpy.npy_ulonglong*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_ulonglong*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_BYTE:
         cyminmax.cminmax[numpy.npy_byte](
-            <numpy.npy_byte*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_byte*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_SHORT:
         cyminmax.cminmax[numpy.npy_short](
-            <numpy.npy_short*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_short*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_INT:
         cyminmax.cminmax[numpy.npy_int](
-            <numpy.npy_int*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_int*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_LONG:
         cyminmax.cminmax[numpy.npy_long](
-            <numpy.npy_long*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_long*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_LONGLONG:
         cyminmax.cminmax[numpy.npy_longlong](
-            <numpy.npy_longlong*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_longlong*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_FLOAT:
         cyminmax.cminmax[numpy.npy_float](
-            <numpy.npy_float*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_float*>numpy.PyArray_DATA(arr), arr_size, out
         )
     elif arr_dtype_num == numpy.NPY_DOUBLE:
         cyminmax.cminmax[numpy.npy_double](
-            <numpy.npy_double*>numpy.PyArray_DATA(arr), arr.size, out
+            <numpy.npy_double*>numpy.PyArray_DATA(arr), arr_size, out
         )
     else:
         raise TypeError("Unsupported type for `arr` of `%s`." % arr.dtype.name)
