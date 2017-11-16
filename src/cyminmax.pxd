16ec7407237485b3f2ff7e64b2d285525fb044f1
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-11-15 20:24:08 -0500

    wip

--- src/cyminmax.pxd
+++ src/cyminmax.pxd
@@ -1,4 +1,5 @@
 cimport cython
+cimport cpython
 cimport numpy
 
 
@@ -42,3 +43,81 @@ cdef inline void cminmax(real* arr_ptr, size_t arr_size, real[:] out) nogil:
 
     out[0] = arr_min
     out[1] = arr_max
+
+
+cdef inline minmax_1d(arr, out):
+    """
+    Computes the minimum and maximum of an array in one pass.
+
+    This is an optimized version of computing the minimum and maximum
+    by doing both in a single pass.
+
+    Args:
+        a(array-like):             array to find min and max of.
+
+    Returns:
+        out(numpy.ndarray):        an array with the min and max values.
+    """
+
+    cdef numpy.NPY_TYPES arr_dtype_num = <numpy.NPY_TYPES>numpy.PyArray_TYPE(
+        arr
+    )
+    cdef size_t arr_size = numpy.PyArray_SIZE(arr)
+    cdef void* arr_data = numpy.PyArray_DATA(arr)
+
+    if arr_dtype_num == numpy.NPY_BOOL:
+        cminmax[numpy.npy_bool](
+            <numpy.npy_bool*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_UBYTE:
+        cminmax[numpy.npy_ubyte](
+            <numpy.npy_ubyte*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_USHORT:
+        cminmax[numpy.npy_ushort](
+            <numpy.npy_ushort*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_UINT:
+        cminmax[numpy.npy_uint](
+            <numpy.npy_uint*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_ULONG:
+        cminmax[numpy.npy_ulong](
+            <numpy.npy_ulong*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_ULONGLONG:
+        cminmax[numpy.npy_ulonglong](
+            <numpy.npy_ulonglong*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_BYTE:
+        cminmax[numpy.npy_byte](
+            <numpy.npy_byte*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_SHORT:
+        cminmax[numpy.npy_short](
+            <numpy.npy_short*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_INT:
+        cminmax[numpy.npy_int](
+            <numpy.npy_int*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_LONG:
+        cminmax[numpy.npy_long](
+            <numpy.npy_long*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_LONGLONG:
+        cminmax[numpy.npy_longlong](
+            <numpy.npy_longlong*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_FLOAT:
+        cminmax[numpy.npy_float](
+            <numpy.npy_float*>arr_data, arr_size, out
+        )
+    elif arr_dtype_num == numpy.NPY_DOUBLE:
+        cminmax[numpy.npy_double](
+            <numpy.npy_double*>arr_data, arr_size, out
+        )
+    else:
+        raise TypeError("Unsupported type for `arr` of `%s`." % arr.dtype.name)
+
+    return out
--- src/cyminmax.pyx
+++ src/cyminmax.pyx
@@ -3,13 +3,23 @@ cimport cyminmax
 cimport cpython
 cimport numpy
 
+import numpy
+
 include "version.pxi"
+
+
+cdef extern from "numpy/arrayobject.h":
+    ctypedef enum NPY_ORDER:
+        NPY_ANYORDER
+        NPY_CORDER
+        NPY_FORTRANORDER
+        NPY_KEEPORDER
 
 
 numpy.import_array()
 
 
-def minmax(a):
+def minmax(a, axis=None, out=None):
     """
     Computes the minimum and maximum of an array in one pass.
 
@@ -26,9 +36,6 @@ def minmax(a):
     cpython.Py_INCREF(a)
     arr = numpy.PyArray_EnsureAnyArray(a)
 
-    cdef numpy.NPY_TYPES arr_dtype_num = <numpy.NPY_TYPES>numpy.PyArray_TYPE(
-        arr
-    )
     cdef size_t arr_size = numpy.PyArray_SIZE(arr)
 
     if not arr_size:
@@ -37,69 +44,44 @@ def minmax(a):
             "identity"
         )
 
-    cdef bint arr_ownsdata = numpy.PyArray_CHKFLAGS(arr, numpy.NPY_OWNDATA)
-    if not arr_ownsdata:
+    if axis is None:
+        axis = tuple(range(arr.ndim))
+
+    try:
+        len(axis)
+    except TypeError:
+        axis = (axis,)
+
+    axis = tuple(reversed(sorted(set(axis))))
+
+    if axis == 0:
+        raise ValueError("`axis` must contain some values if set")
+
+    if len(axis) < arr.ndim:
+        for i, ax in range(len(axis)):
+            arr = numpy.PyArray_SwapAxes(arr, axis, arr.ndim - i - 1)
+        if len(axis) > 1:
+            arr = numpy.PyArray_Reshape(arr, arr.shape[:-len(axis)] + (-1,))
         arr = numpy.PyArray_Copy(arr)
-        arr_ownsdata = True
-
-    cdef void* arr_data = numpy.PyArray_DATA(arr)
-
-    cdef numpy.npy_intp out_shape = 2
-    out = numpy.PyArray_EMPTY(1, &out_shape, arr_dtype_num, 0)
-
-    if arr_dtype_num == numpy.NPY_BOOL:
-        cyminmax.cminmax[numpy.npy_bool](
-            <numpy.npy_bool*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_UBYTE:
-        cyminmax.cminmax[numpy.npy_ubyte](
-            <numpy.npy_ubyte*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_USHORT:
-        cyminmax.cminmax[numpy.npy_ushort](
-            <numpy.npy_ushort*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_UINT:
-        cyminmax.cminmax[numpy.npy_uint](
-            <numpy.npy_uint*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_ULONG:
-        cyminmax.cminmax[numpy.npy_ulong](
-            <numpy.npy_ulong*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_ULONGLONG:
-        cyminmax.cminmax[numpy.npy_ulonglong](
-            <numpy.npy_ulonglong*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_BYTE:
-        cyminmax.cminmax[numpy.npy_byte](
-            <numpy.npy_byte*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_SHORT:
-        cyminmax.cminmax[numpy.npy_short](
-            <numpy.npy_short*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_INT:
-        cyminmax.cminmax[numpy.npy_int](
-            <numpy.npy_int*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_LONG:
-        cyminmax.cminmax[numpy.npy_long](
-            <numpy.npy_long*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_LONGLONG:
-        cyminmax.cminmax[numpy.npy_longlong](
-            <numpy.npy_longlong*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_FLOAT:
-        cyminmax.cminmax[numpy.npy_float](
-            <numpy.npy_float*>arr_data, arr_size, out
-        )
-    elif arr_dtype_num == numpy.NPY_DOUBLE:
-        cyminmax.cminmax[numpy.npy_double](
-            <numpy.npy_double*>arr_data, arr_size, out
-        )
     else:
-        raise TypeError("Unsupported type for `arr` of `%s`." % arr.dtype.name)
+        axis = None
+        arr_flat = numpy.PyArray_Ravel(arr, NPY_KEEPORDER)
+        if not numpy.PyArray_CHKFLAGS(arr, numpy.NPY_OWNDATA):
+            arr_flat = numpy.PyArray_Copy(arr_flat)
+        arr = arr_flat[None]
+
+    if out is None:
+        out = numpy.empty(arr.shape[:-1] + (2,), dtype=arr.dtype)
+    else:
+        if out.shape != (arr.shape[:-1] + (2,)):
+            raise ValueError("`out` did not match expected shape")
+        if out.dtype != arr.dtype:
+            raise ValueError("`out` did not match expected type")
+
+    for i in numpy.ndindex(arr.shape[:-1]):
+        cyminmax.minmax_1d(arr[i], out[i])
+
+    if axis is None:
+        out = out[0]
 
     return out
